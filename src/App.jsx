186f0b583431d1c34508e1920f6ab8bdba485584
vagrant <stan.solomean@gmail.com>
2019-02-13 17:06:33 +0000

    websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ class App extends Component {
     this.addMsg = this.addMsg.bind(this);
 
     this.state = {
-      currentUser: {name: "Don Juan"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      currentUser: {name: "Don Juan"},
       messages: [        
           {
             id: 'id1',
@@ -34,15 +34,79 @@ class App extends Component {
       content: msg
     };
     const newMsgs = [...oldMsgs, newMsgObj];
-    this.setState({messages: newMsgs})
+    this.setState({messages: newMsgs})  
+
+    const message = JSON.parse(msg);
+    const user = JSON.parse(this.state.currentUser.name);
+    this.socket.send(msg);
+
   }
 
   componentDidMount() {
+    this.socket = new WebSocket('ws://localhost:3001');
+
+    // Event listener that is fired when socket is connected
+    this.socket.onopen = () => {
+      console.log('Client connected');
+    };
+ 
+    // this.socket.onmessage = function(event) {
+    //   console.log(e.data);
+
+    //   var f = document.getElementById("chatbox").contentDocument;
+    //   var text = "";
+    //   var msg = JSON.parse(event.data);
+    //   var time = new Date(msg.date);
+    //   var timeStr = time.toLocaleTimeString();
+      
+    //   switch(msg.type) {
+    //     case "id":
+    //       clientID = msg.id;
+    //       setUsername();
+    //       break;
+    //     case "username":
+    //       text = "<b>User <em>" + msg.name + "</em> signed in at " + timeStr + "</b><br>";
+    //       break;
+    //     case "message":
+    //       text = "(" + timeStr + ") <b>" + msg.name + "</b>: " + msg.text + "<br>";
+    //       break;
+    //     case "rejectusername":
+    //       text = "<b>Your username has been set to <em>" + msg.name + "</em> because the name you chose is in use.</b><br>"
+    //       break;
+    //     case "userlist":
+    //       var ul = "";
+    //       for (i=0; i < msg.users.length; i++) {
+    //         ul += msg.users[i] + "<br>";
+    //       }
+    //       document.getElementById("userlistbox").innerHTML = ul;
+    //       break;
+    //   }
+      
+    //   if (text.length) {
+    //     f.write(text);
+    //     document.getElementById("chatbox").contentWindow.scrollByPages(1);
+    //   }
+    // };
+
+
+
+    // Listener for when socket receives a message from the server
+    // The parameter e is a MessageEvent which contains the message from the server along with some metadata.
+    // this.socket.onmessage = e => {
+    //   // the actual message from the server is contained in the `data` key
+    //   console.log(e.data);
+    // };
+
+    // Event listener that is fired when socket is closed 
+    this.onclose = () => {
+      console.log('Client disconnected');
+    };
+
     setTimeout(() => {
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const newMessage = {id: 'id3', username: "Michelle", content: "Hello there!"};
       const messages = this.state.messages.concat(newMessage);
       this.setState({messages, loading: false});
-    }, 500)
+    }, 500);    
   }
 
   render() {
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,17 +1,23 @@
 import React, {Component} from 'react';
 
 function ChatBar ({addMsg, user }) {
-  const enterKey = evt => {
-    if (evt.key == "Enter") {
-      evt.preventDefault();
-      addMsg(evt.target.value);
-      evt.target.value = "";
+  const enterKey = e => {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      addMsg(e.target.value);
+      e.target.value = "";
     }
   }   
+  // const nameChange = e => {
+  //   if (e.key === "Enter") {
+  //     e.preventDefault();
+  //     changeUser = 
+  //   }
+  // }
   return (
     <footer className="chatbar" >
 
-      <input className="chatbar-username" placeholder="Your Name (Optional)"  defaultValue={user.name}/>
+      <input className="chatbar-username" placeholder="Your Name (Optional)" defaultValue={user.name}/>
       <input className="chatbar-message" name="msg" onKeyPress={enterKey} placeholder="Type a message and hit ENTER" />
 
     </footer>    
