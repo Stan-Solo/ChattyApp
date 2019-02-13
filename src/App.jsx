4f57db10139b326ec418d35888906337e07c5c4b
vagrant <stan.solomean@gmail.com>
2019-02-13 01:36:21 +0000

    w6d2 exercises done

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,67 @@
 import React, {Component} from 'react';
 import ChatBar from './ChatBar.jsx';
 import MessageList from './MessageList.jsx';
-
+import {generateRandomId} from './gen-random.js';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.addMsg = this.addMsg.bind(this);
+
+    this.state = {
+      currentUser: {name: "Don Juan"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [        
+          {
+            id: 'id1',
+            username: "Bob",
+            content: "Has anyone seen my marbles?",
+          },
+          {
+            id: 'id2',
+            username: "Charles",
+            content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+          }
+        ],
+      loading: true,
+    };
+  }
+
+  addMsg(msg) {
+    const oldMsgs = this.state.messages;
+    const newMsgObj = {
+      id: generateRandomId(),
+      username: this.state.currentUser.name,
+      content: msg
+    };
+    const newMsgs = [...oldMsgs, newMsgObj];
+    this.setState({messages: newMsgs})
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
+      const messages = this.state.messages.concat(newMessage);
+      this.setState({messages, loading: false});
+    }, 500)
+  }
+
   render() {
-    return (
+      if (this.state.loading) {
+        return <h1> Loading, please wait...</h1>
+      } else {
+        return (  
         <div>
 
           <nav className="navbar">
             <a href="/" className="navbar-brand">Chatty</a>
           </nav>
 
-          <MessageList />
-          <ChatBar />
+          <MessageList messages={this.state.messages} />
+          <ChatBar addMsg={this.addMsg} user={this.state.currentUser} />
 
         </div>
-
-      )
+        )
+      }
     }
   }
   export default App;
--- src/ChatBar.jsx
+++ src/ChatBar.jsx
@@ -1,13 +1,20 @@
 import React, {Component} from 'react';
 
-class ChatBar extends Component {
-  render() {
-    return (
-      <footer className="chatbar">
-        <input className="chatbar-username" placeholder="Your Name (Optional)" />
-        <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
-      </footer>    
-    );
-  }
+function ChatBar ({addMsg, user }) {
+  const enterKey = evt => {
+    if (evt.key == "Enter") {
+      evt.preventDefault();
+      addMsg(evt.target.value);
+      evt.target.value = "";
+    }
+  }   
+  return (
+    <footer className="chatbar" >
+
+      <input className="chatbar-username" placeholder="Your Name (Optional)"  defaultValue={user.name}/>
+      <input className="chatbar-message" name="msg" onKeyPress={enterKey} placeholder="Type a message and hit ENTER" />
+
+    </footer>    
+  );
 }
 export default ChatBar;
